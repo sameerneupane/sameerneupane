995e9d24fafa20e5d63080ac9af8c4bf6a0dc64a
sameerneupane <100555467+sameerneupane@users.noreply.github.com>
2022-07-09 08:47:14 +0545

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
 
+from web3 import Web3, HTTPProvider
+
+# Get your node JSON-RPC URL
+json_rpc_url = input("Please enter your Ethereum mainnet JSON-RPC URL here")
+web3 = Web3(HTTPProvider(json_rpc_url))
